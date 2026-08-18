f449b34634488d715bf8679dde1e74d68896223f
believe2108 <tema_1288@mail.ru>
2026-08-18 17:28:00 +0300

    Task1

--- Day2/task01.py
+++ Day2/task01.py
@@ -1,11 +1,20 @@
 import asyncio
 import random
+import time
 
-async def wait_random():
+async def wait_random(num: int) -> None:
     delay = random.randint(1, 5)
     await asyncio.sleep(delay)
-    print(f"Задача завершилась через {delay} секунд")
+    print(f"Задача корутина {num} завершилась через {delay} секунд")
 
 
-# TODO: дана асинхронная функция которая ждет случайное количество секунд (от 1 до 5)
-#  Создайте 3 корутины, и запустите их асинхронно с помощью asyncio.gather().
+async def main():
+    coro1 = asyncio.create_task(wait_random(1))
+    await asyncio.gather(wait_random(2),coro1,wait_random(3))
+
+
+if __name__ == "__main__":
+    start = time.perf_counter()
+    asyncio.run(main())
+    end = time.perf_counter()
+    print(f'Время выполнение заняло: {end-start:.2f} c.')
