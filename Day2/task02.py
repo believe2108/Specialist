04fb0d41d5a1c690d311df0757effd6110a013fe
believe2108 <tema_1288@mail.ru>
2026-08-18 17:38:19 +0300

    Task2

--- Day2/task02.py
+++ Day2/task02.py
@@ -1,2 +1,22 @@
-# TODO: Создайте 4 корутины, каждая из которых возвращает строку.
-#  Запустите их параллельно и соберите результаты в список.
+import asyncio
+import time
+from typing import List
+
+async def get_str(num: int) -> str:
+    await asyncio.sleep(1)
+    return f'Я - корутин {num}'
+
+
+async def main() -> List[str]:
+    tasks = [asyncio.create_task(get_str(n)) for n in range(4)]
+    return [await task for task in tasks]
+
+
+if __name__ == "__main__":
+    start = time.perf_counter()
+    results = asyncio.run(main())
+    end = time.perf_counter()
+    print('Результат:')
+    for result in results:
+        print(result)
+    print(f'Время выполнение заняло: {end-start:.2f} c.')
