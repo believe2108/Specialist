e01a059cc0ad3580ee28ae6cd8ad40a4eb208fdd
believe2108 <tema_1288@mail.ru>
2026-08-18 21:32:02 +0300

    'Task5'

--- Day2/task05.py
+++ Day2/task05.py
@@ -1,2 +1,29 @@
-# TODO: Создайте корутину, которая ждет 5 секунд.
-#  Запустите ее как задачу и отмените ее через 2 секунды.
+import asyncio
+import time
+
+
+async def wait_for() -> None:
+    print(f"Моя задача ждать 5 секунд")
+    await asyncio.sleep(5)
+
+
+async def main() -> None:
+    task = asyncio.create_task(wait_for())
+    await asyncio.sleep(2)
+    task.cancel()
+    print('Отменил через 2 секунды')
+    while not task.cancelled():
+        print('Задача еще не отменена. Жду секунду')
+        await asyncio.sleep(1)
+    print(f'Задача отменена')
+    # try:
+    #     await task
+    # except asyncio.CancelledError:
+    #     print('Задание было отменено')
+
+
+if __name__ == "__main__":
+    start = time.perf_counter()
+    asyncio.run(main())
+    end = time.perf_counter()
+    print(f'Время выполнение заняло: {end-start:.2f} c.')
