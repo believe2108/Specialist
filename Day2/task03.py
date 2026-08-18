982f201f24f905579691cac2ab423a2e50d193fa
believe2108 <tema_1288@mail.ru>
2026-08-18 17:50:33 +0300

    Task3

--- Day2/task03.py
+++ Day2/task03.py
@@ -1,3 +1,21 @@
-# TODO: Создайте несколько асинхронных задач с разными задержками.
-#  Используйте asyncio.as_completed() для обработки результатов задач по мере их завершения.
-#  Выведите результаты каждой задачи сразу после ее завершения.
+import asyncio
+import time
+
+
+async def wait_random(num: int) -> str:
+    await asyncio.sleep(num)
+    return f"Задача корутина {num} завершилась через {num} секунд"
+
+
+async def main() -> None:
+    tasks = [asyncio.create_task(wait_random(n)) for n in range(4,0,-1)]
+    # [4,3,2,1]
+    for task in asyncio.as_completed(tasks):
+        print(await task)
+
+
+if __name__ == "__main__":
+    start = time.perf_counter()
+    asyncio.run(main())
+    end = time.perf_counter()
+    print(f'Время выполнение заняло: {end-start:.2f} c.')
